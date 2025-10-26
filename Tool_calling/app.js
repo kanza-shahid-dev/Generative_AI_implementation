197a5f6e2e5b035d81141a3273ffddc5cf9b6d13
kanza-shahid-dev <kanzashahid18@gmail.com>
2025-10-26 13:49:39 +0500

    updated tool calling

--- Tool_calling/app.js
+++ Tool_calling/app.js
@@ -6,57 +6,69 @@ const qroq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
 async function main() {
-  const completions = await qroq.chat.completions.create({
-    temperature: 0,
-    model: "llama-3.3-70b-versatile",
-    messages: [
-      {
-        role: "system",
-        content: `If the user asks about current, trending, latest or real-time information,
+  const messages = [
+    {
+      role: "system",
+      content: `If the user asks about current, trending, latest or real-time information,
       ALWAYS call the function "webSearch". Otherwise, answer normally.`,
-      },
-      {
-        role: "user",
-        content: "Tell about latest iphone", //Current weather in islamabad
-      },
-    ],
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description: "search latest information from web and real time data",
-          parameters: {
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description:
-                  "The search query to find relevant information from the web",
+    },
+    {
+      role: "user",
+      content: "Tell about latest iphone", //Current weather in Islamabad
+    },
+  ];
+
+  while (true) {
+    const completions = await qroq.chat.completions.create({
+      temperature: 0,
+      model: "llama-3.3-70b-versatile",
+      messages: messages,
+      tools: [
+        {
+          type: "function",
+          function: {
+            name: "webSearch",
+            description:
+              "search latest information from web and real time data",
+            parameters: {
+              type: "object",
+              properties: {
+                query: {
+                  type: "string",
+                  description:
+                    "The search query to find relevant information from the web",
+                },
               },
+              required: ["query"],
             },
-            required: ["query"],
           },
         },
-      },
-    ],
-    tool_choice: "required", // means some times model can decide to use tool or not // also can be set to "always" or "never"
-  });
-  //console.log(JSON.stringify(completions.choices[0].message));
+      ],
+      tool_choice: "auto", // means some times model can decide to use tool or not // also can be set to "always" or "never"
+    });
 
-  const toolCalls = completions.choices[0].message.tool_calls;
-  if (!toolCalls) {
-    console.log("Response from AI", completions.choices[0].message.content);
-    return;
-  }
-  for (const toolCall of toolCalls) {
-    // console.log("Tool call:", toolCall);
-    const functionName = toolCall.function.name;
-    const functionArgs = toolCall.function.arguments;
+    messages.push(completions.choices[0].message);
 
-    if (functionName === "webSearch") {
-      const toolResponse = await webSearch(JSON.parse(functionArgs));
-      console.log("Tool response:", toolResponse);
+    const toolCalls = completions.choices[0].message.tool_calls;
+    if (!toolCalls) {
+      console.log("Response from AI", completions.choices[0].message.content);
+      break;
+    }
+    for (const toolCall of toolCalls) {
+      const functionName = toolCall.function.name;
+      const functionArgs = toolCall.function.arguments;
+
+      if (functionName === "webSearch") {
+        const toolResponse = await webSearch(JSON.parse(functionArgs));
+        console.log("Tool response:", toolResponse);
+
+        messages.push({
+          role: "tool",
+          tool_call_id: toolCall.id,
+          name: functionName,
+          content: toolResponse,
+        });
+      }
     }
   }
 }
@@ -64,8 +76,10 @@ async function main() {
 main();
 
 async function webSearch({ query }) {
+  console.log("calling web search");
+
   const response = await tvly.search(query);
-  console.log("Tavily search response:", response);
+  //  console.log("Tavily search response:", response); //it will be multiple resources// for understanding see the response structure
   const results = response.results.map((res) => res.content).join("\n\n");
   return JSON.stringify(results);
 }
